910f49b106b711b60bec3cdd0e5cd76b8650e858
juanfish <juanfish@hotmail.com>
2024-06-22 13:27:41 -0400

    primer commit

---
+++ instalador git.bat
@@ -0,0 +1,8 @@
+echo *.dcu > ".gitignore"
+echo *.cfg >> ".gitignore"
+echo *.dof >> ".gitignore"
+echo *.res >> ".gitignore"
+echo *.exe >> ".gitignore"
+echo Informacion del proyecto > README.MD
+git init
+pause
